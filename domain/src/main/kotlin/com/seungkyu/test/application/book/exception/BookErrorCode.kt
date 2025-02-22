859b0dfe01dbbf45db7b84f009ea13153cd66f3d
Seungkyu-Han <trust1204@gmail.com>
2025-02-22 22:59:22 +0900

    feat: add book application exception

---
+++ domain/src/main/kotlin/com/seungkyu/test/application/book/exception/BookErrorCode.kt
@@ -0,0 +1,11 @@
+package com.seungkyu.test.application.book.exception
+
+import org.springframework.http.HttpStatus
+
+enum class BookErrorCode(
+    val httpStatus: HttpStatus,
+    val message: String
+) {
+
+    INVALID_ISBN(HttpStatus.BAD_REQUEST, "유효하지 않은 ISBN입니다."),
+}
