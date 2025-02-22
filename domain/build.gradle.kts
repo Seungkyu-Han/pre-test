ac9780bd9caf2534dbcd2f6681f496a77563d64c
Seungkyu-Han <trust1204@gmail.com>
2025-02-22 18:07:33 +0900

    feat: valid email check

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -9,6 +9,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework:spring-tx:6.2.3")
 
--- domain/src/main/kotlin/com/seungkyu/test/application/author/dto/UpdateAuthorCommand.kt
+++ domain/src/main/kotlin/com/seungkyu/test/application/author/dto/UpdateAuthorCommand.kt
@@ -1,7 +1,10 @@
 package com.seungkyu.test.application.author.dto
 
+import jakarta.validation.constraints.Email
+
 data class UpdateAuthorCommand(
     val id: Int,
     val name: String,
+    @Email(message = "유효한 이메일 형식이 아닙니다")
     val email: String
 )
