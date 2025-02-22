87f7249d3acdae3f170696afc10e81a35013dbaf
Seungkyu-Han <trust1204@gmail.com>
2025-02-22 23:10:44 +0900

    feat: add book domain application service

---
+++ domain/src/main/kotlin/com/seungkyu/test/application/book/ports/input/service/BookService.kt
@@ -0,0 +1,20 @@
+package com.seungkyu.test.application.book.ports.input.service
+
+import com.seungkyu.test.application.book.dto.*
+import jakarta.validation.Valid
+
+interface BookService {
+
+    fun createBook(@Valid createBookCommand: CreateBookCommand): CreateBookResponse
+
+    fun bookInfo(bookId: Int): BookInfoResponse
+
+    fun booksInfo(page: Int, pageSize: Int, bookSortEnum: BookSortEnum): BookInfoResponses
+
+    fun updateAuthor(
+        id: Int,
+        @Valid updateBookCommand: UpdateBookCommand
+    ): BookInfoResponse
+
+    fun deleteBook(bookId: Int)
+}
