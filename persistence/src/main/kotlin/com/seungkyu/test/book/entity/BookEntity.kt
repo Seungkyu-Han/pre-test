f044e46e122fe6c01f4324905e64a394944e2fa1
Seungkyu-Han <trust1204@gmail.com>
2025-02-22 23:19:57 +0900

    feat: add book entity

---
+++ persistence/src/main/kotlin/com/seungkyu/test/book/entity/BookEntity.kt
@@ -0,0 +1,17 @@
+package com.seungkyu.test.book.entity
+
+import jakarta.persistence.*
+import java.time.LocalDate
+
+@Entity(name = "books")
+data class BookEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int?,
+    val title: String,
+    val description: String,
+    @Column(unique = true)
+    val isbn: String,
+    val publicationDate: LocalDate,
+    val authorId: Int,
+)
