6dcb40b951e1377e628103c8c038af605bb44949
Seungkyu-Han <trust1204@gmail.com>
2025-02-23 14:49:13 +0900

    docs: book dto swagger description

--- domain/src/main/kotlin/com/seungkyu/test/application/book/dto/CreateBookCommand.kt
+++ domain/src/main/kotlin/com/seungkyu/test/application/book/dto/CreateBookCommand.kt
@@ -1,11 +1,17 @@
 package com.seungkyu.test.application.book.dto
 
+import jakarta.validation.constraints.NotNull
 import java.time.LocalDate
 
 data class CreateBookCommand(
-    val title: String,
-    val description: String,
-    val isbn: String,
-    val publicationDate: LocalDate,
-    val authorId: Int,
+    @field:NotNull(message = "이름을 입력해주세요")
+    val title: String? = null,
+    @field:NotNull(message = "설명을 입력해주세요")
+    val description: String? = null,
+    @field:NotNull(message = "isbn을 입력해주세요")
+    val isbn: String? = null,
+    @field:NotNull(message = "발행일을 입력해주세요")
+    val publicationDate: LocalDate? = null,
+    @field:NotNull(message = "작가의 id를 입력해주세요")
+    val authorId: Int? = null,
 )
